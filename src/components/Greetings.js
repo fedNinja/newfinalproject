1981fdcfd40019d00b8b1148ac9eb1d1f9b66403
payal agrawal <pagrawal312@gmail.com>
2017-03-19 21:48:40 -0700

    signup form and it state

---
+++ src/components/Greetings.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Greetings extends Component{
+  render(){
+    return(
+      <div className="jumbotron">
+        <h1>Lets finish the chores and start earning!</h1>
+      </div>
+    );
+  }
+}
+export default Greetings;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+
+import routes from './routes';
 
 ReactDOM.render(
-      <div>
-      Hello World!
-      </div>, document.getElementById('root')
+      <Router history={browserHistory} routes={routes}/>, document.getElementById('root')
 )
